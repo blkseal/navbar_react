57dc074e9fc56660f02232bd872eea1c987619b5
Black Seal <hugosfiuza@gmail.com>
2025-02-22 12:27:08 +0000

    First NavbarReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import instagramIcon from './images/instagram.svg';
+import linkedinIcon from './images/linkedin.svg';
+import { useState } from 'react';
+
+function NavBarHeader({ name, title }) {
+  return <>
+    <div className="navbarHeader">
+      <h1 className=
+        "navbarName">{name}</h1>
+      <h2 className="navbarTitle">{title}</h2>
+    </div>
+  </>
+}
+
+function NavBarButtons() {
+  const [activeButton, setActiveButton] = useState('Home');
+
+  return <>
+    <div className="navbarButtons">
+      <button 
+        className={activeButton === 'Home' ? 'active' : ''} 
+        onClick={() => setActiveButton('Home')}
+      > Home </button>
+      <button 
+        className={activeButton === 'About' ? 'active' : ''} 
+        onClick={() => setActiveButton('About')}
+      > About </button>
+      <button 
+        className={activeButton === 'My Work' ? 'active' : ''} 
+        onClick={() => setActiveButton('My Work')}
+      > My Work </button>
+      <button 
+        className={activeButton === 'Group Work' ? 'active' : ''} 
+        onClick={() => setActiveButton('Group Work')}
+      > Group Work </button>
+      <button 
+        className={activeButton === 'Contact' ? 'active' : ''} 
+        onClick={() => setActiveButton('Contact')}
+      > Contact </button>
+    </div>
+  </>
+}
+
+function NavBarLinks({ instagram, linkedin }) {
+  return <>
+    <div className="navbarLinks">
+    <img src={instagramIcon} onClick={() => window.open(instagram)} alt="Instagram" />
+      <img src={linkedinIcon} onClick={() => window.open(linkedin)} alt="LinkedIn" />
+    </div>
+  </>
+}
+
+function NavBar({ name, title, instagram, linkedin }) {
+  return <>
+    <div className="navbar">
+      <NavBarHeader name={name} title={title} />
+      <NavBarButtons />
+      <NavBarLinks instagram={instagram} linkedin={linkedin} />
+    </div>
+  </>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = "Hugo Fiuza"
+  const title = "Full-Stack Developer"
+  const instagram = "https://www.instagram.com/hugofiuza/"
+  const linkedin = "https://www.linkedin.com/in/hugofiuza/"
+  return <>
+    <NavBar name={name} title={title} instagram={instagram} linkedin={linkedin} />
+  </>
 }
 
 export default App;
